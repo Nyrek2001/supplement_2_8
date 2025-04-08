84f8a5cb98536564e9da3e1c9b243e9ec19e3847
Nyrek2001 <Nyrekwheeler@gmail.com>
2025-04-08 15:13:11 -0400

    Initial Rust library project setup for mathlib

---
+++ target/debug/deps/mathlib-caf9424a5e83a766.d
@@ -0,0 +1,7 @@
+C:\Users\mcelr\mathlib\target\debug\deps\libmathlib-caf9424a5e83a766.rmeta: src\lib.rs
+
+C:\Users\mcelr\mathlib\target\debug\deps\libmathlib-caf9424a5e83a766.rlib: src\lib.rs
+
+C:\Users\mcelr\mathlib\target\debug\deps\mathlib-caf9424a5e83a766.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/mathlib-d40b92538fdc0b66.d
@@ -0,0 +1,5 @@
+C:\Users\mcelr\mathlib\target\debug\deps\mathlib-d40b92538fdc0b66.exe: src\lib.rs
+
+C:\Users\mcelr\mathlib\target\debug\deps\mathlib-d40b92538fdc0b66.d: src\lib.rs
+
+src\lib.rs:
